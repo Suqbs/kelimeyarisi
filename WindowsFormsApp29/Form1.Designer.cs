fdd441fe2ee2eabec5fcf669da3e6eb13129eed8
Furkan KARADENİZ <furrkaradenizz@gmail.com>
2022-05-01 22:20:48 +0300

    Projeyi commit etim.

---
+++ WindowsFormsApp29/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApp29
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Lblpuan = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Lbltime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Lblpuan
+            // 
+            this.Lblpuan.AutoSize = true;
+            this.Lblpuan.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lblpuan.Location = new System.Drawing.Point(219, 42);
+            this.Lblpuan.Name = "Lblpuan";
+            this.Lblpuan.Size = new System.Drawing.Size(32, 33);
+            this.Lblpuan.TabIndex = 15;
+            this.Lblpuan.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(114, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 33);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Puan:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.listBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(180, 334);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(459, 142);
+            this.listBox1.TabIndex = 13;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(346, 281);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(126, 47);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "İsim Ekle";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(358, 224);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // Lbltime
+            // 
+            this.Lbltime.BackColor = System.Drawing.Color.Gray;
+            this.Lbltime.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Lbltime.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Lbltime.Location = new System.Drawing.Point(564, 23);
+            this.Lbltime.Name = "Lbltime";
+            this.Lbltime.Size = new System.Drawing.Size(120, 73);
+            this.Lbltime.TabIndex = 10;
+            this.Lbltime.Text = "00";
+            this.Lbltime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(346, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 47);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Harf Seçin";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "B.png");
+            this.ımageList1.Images.SetKeyName(1, "Y.png");
+            this.ımageList1.Images.SetKeyName(2, "K.png");
+            this.ımageList1.Images.SetKeyName(3, "S.png");
+            this.ımageList1.Images.SetKeyName(4, "O.png");
+            this.ımageList1.Images.SetKeyName(5, "M.png");
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Location = new System.Drawing.Point(346, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(126, 116);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(781, 517);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Lblpuan);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Lbltime);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Lblpuan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Lbltime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp29/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp29
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Kısıtlı süre içerisinde yazabildiğin kadar kelime yaz!\n\nKurallar:\n1. Süre bitmeden en az bir kelime yazmalısın.\n2. Aynı kelimeyi yazmak oyunun bitmesi demektir.\n\nİyi şanslar!");
+            button2.Enabled = false;
+        }
+        
+        //Input olarak sadece harf ve control (backspace, ctrl vs.) değerlerini alır. 
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        //global değerler
+        int puan = 0;
+        int index;
+        int listboxDegeri1;
+        int listboxDegeri2;
+        int zaman = 60;
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            zaman = 60;
+            timer1.Start();
+            listboxDegeri1 = listBox1.Items.Count;
+            button1.Enabled = false;
+            button2.Enabled = true;
+
+            //Random atanan bir index değeriyle ımageListten bir string değeri alıp pictureBox1'e yazdırdı.
+            Random random = new Random();
+            index = random.Next(ımageList1.Images.Count);
+            string imageName = ımageList1.Images.Keys[index];
+            pictureBox1.Image = ımageList1.Images[imageName];
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            zaman--;
+
+            if (zaman == 0)
+            {
+                timer1.Stop();
+                listboxDegeri2 = listBox1.Items.Count;
+                button1.Enabled = true;
+                button2.Enabled = false;
+
+                //Harf seç butonuyla aldığımız listboxDegeri1 ile zaman == 0 olduğunda aldığımız listboxDegeri2 'i karşılaştırıyoruz. Eğer eşitlerse herhangi bir kelime eklenmediği için puan = 0 oluyor.
+                if (zaman == 0 && listboxDegeri1 == listboxDegeri2) 
+                {
+                    puan = 0;
+                    MessageBox.Show("Kelime girmediğiniz için kaybettiniz.");
+                    listBox1.Items.Clear();
+                }
+            }
+            Lbltime.Text = Convert.ToString(zaman);
+            Lblpuan.Text = Convert.ToString(puan);
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string kelime = textBox1.Text.ToLower(); //ToLower metodu: büyük-küçük harf duyarlılığını ortadan kaldırır.
+
+            //Dosya yolunu kendine göre girmelisin.
+            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/Y.txt")) // StreamReader fonksiyonu ile okunacak dosyamızı açtırıyoruz. Kelime başına ortalama 4000 satırdır.
+            {
+                string satir; // Burada okuduğunuz her satırı atamamız için gerekli değişkeni tanımlıyoruz.
+
+                while ((satir = sr.ReadLine()) != null) // Döngü kurup; eğer satır boş değilse, satir değişkenine ekleme yapıyoruz.
+                {
+                    if (index == 1 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
+                    {
+                        textBox1.Text = "";
+                        listBox1.Items.Add(satir);
+                        puan += 5;
+                    }
+                }
+            }
+
+            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/K.txt"))
+            {
+                string satir;
+
+                while ((satir = sr.ReadLine()) != null)
+                {
+                    if (index == 2 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
+                    {
+                        textBox1.Text = "";
+                        listBox1.Items.Add(satir);
+                        puan += 5;
+                    }
+                }
+            }
+
+            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/S.txt"))
+            {
+                string satir;
+
+                while ((satir = sr.ReadLine()) != null)
+                {
+                    if (index == 3 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
+                    {
+                        textBox1.Text = "";
+                        listBox1.Items.Add(satir);
+                        puan += 5;
+                    }
+                }
+            }
+
+            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/O.txt"))
+            {
+                string satir;
+
+                while ((satir = sr.ReadLine()) != null)
+                {
+                    if (index == 4 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
+                    {
+                        textBox1.Text = "";
+                        listBox1.Items.Add(satir);
+                        puan += 5;
+                    }
+                }
+            }
+
+            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/M.txt"))
+            {
+                string satir;
+
+                while ((satir = sr.ReadLine()) != null)
+                {
+                    if (index == 5 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
+                    {
+                        textBox1.Text = "";
+                        listBox1.Items.Add(satir);
+                        puan += 5;
+                    }
+                }
+            }
+
+            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/B.txt"))
+            {
+                string satir;
+
+                while ((satir = sr.ReadLine()) != null)
+                {
+                    if (index == 0 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
+                    {
+                        textBox1.Text = "";
+                        listBox1.Items.Add(satir);
+                        puan += 5;
+                    }
+                }
+            }
+
+            //Eğer listboxa aynı değeri girdiysek bunları gerçekleştir. Bunu eventin sonunda yazmamın sebebi, eğer kelime tekrar yazılırken kelimenin baş harfi resimdeki harfle aynıysa while döngüsüne bir daha girip listbox'a onu giriyor. Bunu sonda yazmak o gelen değeri de siliyor. 
+            if (listBox1.Items.Contains(textBox1.Text.ToLower()))
+            {
+                puan = 0;
+                button1.Enabled = true;
+                timer1.Stop();
+                textBox1.Text = "";
+                MessageBox.Show("Aynı kelimeyi girdiğin için kaybettin. Tekrar başlamak için harf seç.");
+                listBox1.Items.Clear();
+            }
+            Lblpuan.Text = Convert.ToString(puan);
+        }
+    }
+}
