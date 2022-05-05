b7e85c0ec544caeddad86bbed325f0f0a9b9abac
Furkan KARADENİZ <furrkaradenizz@gmail.com>
2022-05-05 17:54:40 +0300

    Kodlar refactor edildi. Bazı buglar düzeltildi.

--- WindowsFormsApp29/Form1.cs
+++ WindowsFormsApp29/Form1.cs
@@ -23,16 +23,17 @@ namespace WindowsFormsApp29
             e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar);
         }
 
-        //global değerler
+        //Global değerler
         int puan = 0;
-        int index;
         int listboxDegeri1;
         int listboxDegeri2;
         int zaman = 60;
 
         private void button1_Click_1(object sender, EventArgs e)
         {
+            int index;
             zaman = 60;
+            textBox1.Clear();
             timer1.Start();
             listboxDegeri1 = listBox1.Items.Count;
             button1.Enabled = false;
@@ -41,8 +42,7 @@ namespace WindowsFormsApp29
             //Random atanan bir index değeriyle ımageListten bir string değeri alıp pictureBox1'e yazdırdı.
             Random random = new Random();
             index = random.Next(ımageList1.Images.Count);
-            string imageName = ımageList1.Images.Keys[index];
-            pictureBox1.Image = ımageList1.Images[imageName];
+            pictureBox1.Image = ımageList1.Images[index];
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -61,6 +61,7 @@ namespace WindowsFormsApp29
                 {
                     puan = 0;
                     MessageBox.Show("Kelime girmediğiniz için kaybettiniz.");
+                    textBox1.Text = string.Empty;
                     listBox1.Items.Clear();
                 }
             }
@@ -68,19 +69,18 @@ namespace WindowsFormsApp29
             Lblpuan.Text = Convert.ToString(puan);
         }
 
-
-        private void button2_Click(object sender, EventArgs e)
+        private void Kelime_Datasini_Al(string harftxt)
         {
             string kelime = textBox1.Text.ToLower(); //ToLower metodu: büyük-küçük harf duyarlılığını ortadan kaldırır.
 
             //Dosya yolunu kendine göre girmelisin.
-            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/Y.txt")) // StreamReader fonksiyonu ile okunacak dosyamızı açtırıyoruz. Kelime başına ortalama 4000 satırdır.
+            using (StreamReader sr = new StreamReader(@"C:\Users\windows1\source\repos\Suqbs\kelimeyarisi\WindowsFormsApp29\Resources\" + harftxt)) // StreamReader fonksiyonu ile okunacak dosyamızı açtırıyoruz. Kelime başına ortalama 4000 satırdır.
             {
                 string satir; // Burada okuduğunuz her satırı atamamız için gerekli değişkeni tanımlıyoruz.
 
                 while ((satir = sr.ReadLine()) != null) // Döngü kurup; eğer satır boş değilse, satir değişkenine ekleme yapıyoruz.
                 {
-                    if (index == 1 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
+                    if (satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
                     {
                         textBox1.Text = "";
                         listBox1.Items.Add(satir);
@@ -88,93 +88,33 @@ namespace WindowsFormsApp29
                     }
                 }
             }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string kelime = textBox1.Text.ToLower(); //ToLower metodu: büyük-küçük harf duyarlılığını ortadan kaldırır.
 
-            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/K.txt"))
-            {
-                string satir;
+            Kelime_Datasini_Al("Y.txt");
+            Kelime_Datasini_Al("K.txt");
+            Kelime_Datasini_Al("M.txt");
+            Kelime_Datasini_Al("O.txt");
+            Kelime_Datasini_Al("S.txt");
+            Kelime_Datasini_Al("Y.txt");
 
-                while ((satir = sr.ReadLine()) != null)
-                {
-                    if (index == 2 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
-                    {
-                        textBox1.Text = "";
-                        listBox1.Items.Add(satir);
-                        puan += 5;
-                    }
-                }
-            }
-
-            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/S.txt"))
-            {
-                string satir;
-
-                while ((satir = sr.ReadLine()) != null)
-                {
-                    if (index == 3 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
-                    {
-                        textBox1.Text = "";
-                        listBox1.Items.Add(satir);
-                        puan += 5;
-                    }
-                }
-            }
-
-            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/O.txt"))
-            {
-                string satir;
-
-                while ((satir = sr.ReadLine()) != null)
-                {
-                    if (index == 4 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
-                    {
-                        textBox1.Text = "";
-                        listBox1.Items.Add(satir);
-                        puan += 5;
-                    }
-                }
-            }
-
-            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/M.txt"))
-            {
-                string satir;
-
-                while ((satir = sr.ReadLine()) != null)
-                {
-                    if (index == 5 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
-                    {
-                        textBox1.Text = "";
-                        listBox1.Items.Add(satir);
-                        puan += 5;
-                    }
-                }
-            }
-
-            using (StreamReader sr = new StreamReader("C:/Users/windows1/Desktop/WindowsFormsApp29/WindowsFormsApp29/Resources/B.txt"))
-            {
-                string satir;
-
-                while ((satir = sr.ReadLine()) != null)
-                {
-                    if (index == 0 && satir == kelime && !listBox1.Items.Contains(textBox1.Text.ToLower()))
-                    {
-                        textBox1.Text = "";
-                        listBox1.Items.Add(satir);
-                        puan += 5;
-                    }
-                }
-            }
 
             //Eğer listboxa aynı değeri girdiysek bunları gerçekleştir. Bunu eventin sonunda yazmamın sebebi, eğer kelime tekrar yazılırken kelimenin baş harfi resimdeki harfle aynıysa while döngüsüne bir daha girip listbox'a onu giriyor. Bunu sonda yazmak o gelen değeri de siliyor. 
             if (listBox1.Items.Contains(textBox1.Text.ToLower()))
             {
                 puan = 0;
+                zaman = 60;
                 button1.Enabled = true;
                 timer1.Stop();
                 textBox1.Text = "";
                 MessageBox.Show("Aynı kelimeyi girdiğin için kaybettin. Tekrar başlamak için harf seç.");
+                textBox1.Text = string.Empty;
                 listBox1.Items.Clear();
             }
             Lblpuan.Text = Convert.ToString(puan);
+            Lbltime.Text = Convert.ToString(zaman);
         }
     }
 }
